ebfbc2f45fefea3c4d13e3a65b4cd3d32f0e6e3c
qc <qiuchen610714354@gmail.com>
2026-01-30 18:28:18 +0800

    feat: 兼容 rag 和大模型能力

--- llamaidx.py
+++ llamaidx.py
@@ -1,7 +1,7 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, PromptTemplate
 from llama_index.llms.openai_like import OpenAILike
-from llama_index.core import Settings
 from defEmbedding import QwenEmbedding
+
 # from llama_index.embeddings.openai_like import OpenAILikeEmbedding
 llm = OpenAILike(
     model="qwen3-max",                     # 或 qwen-turbo / qwen-max
@@ -20,7 +20,13 @@ Settings.embed_model = embed_model
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 
-query_engine = index.as_query_engine()
-response = query_engine.query("清炒土豆丝怎么做？")
+query_engine = index.as_query_engine(text_qa_template=PromptTemplate("""
+    你是一个只能助手，请优先参考提供的上下文进行回答
+    如果上下文不足，可以使用你自身知识补充，但要说明哪些来自上下文，哪些是常识。
+    上下文：{context_str}
+    问题：{query_str}
+    回答：
+"""))
+response = query_engine.query("如何关闭车窗？")
 
 print(response)
